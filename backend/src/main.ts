a61cea78942685f6faeeeb992994011e07a23617
Sofia Klimova <klimovasonya@gmail.com>
2023-04-05 11:32:29 +0300

    .

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,21 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import config from '../config';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.enableCors();
+  app.use(
+    cors({
+      origin: [
+        'https://klmvsn.nomoredomains.monster',
+        'http://klmvsn.nomoredomains.monster',
+      ],
+      methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    }),
+  );
   await app.listen(config().port);
 }
 bootstrap();
