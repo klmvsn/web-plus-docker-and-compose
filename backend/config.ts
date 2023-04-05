2439a4745486346ce246c19b1792b21336577454
Sofia Klimova <klimovasonya@gmail.com>
2023-04-05 11:57:18 +0300

    fix

--- backend/config.ts
+++ backend/config.ts
@@ -2,7 +2,7 @@ export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   database: {
     type: process.env.DATABASE_TYPE,
-    host: process.env.POSTGRES_HOST,
+    host: process.env.POSTGRES_HOST || 'database',
     port: parseInt(process.env.POSTGRES_PORT, 10) || 5432,
     database: process.env.POSTGRES_DB,
     username: process.env.POSTGRES_USER,
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,21 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import config from '../config';
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  app.use(
-    cors({
-      origin: [
-        'https://klmvsn.nomoredomains.monster',
-        'http://klmvsn.nomoredomains.monster',
-      ],
-      methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
+  const corsOpt = {
+    origin: [
+      'https://klmvsn.nomoredomains.monster',
+      'http://klmvsn.nomoredomains.monster',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  };
+  app.enableCors(corsOpt);
   await app.listen(config().port);
 }
 bootstrap();
